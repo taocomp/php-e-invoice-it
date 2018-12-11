dcb6033edf218183e71a1944f62799a4eb8abda9
aded <aded@aded.it>
2018-12-11 06:15:53 +0100

    Add ini_set soap to example

--- examples/invoice.php
+++ examples/invoice.php
@@ -19,6 +19,9 @@
  * along with php-sdicoop-invoice.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+ini_set("soap.wsdl_cache_enabled", 0);
+ini_set('soap.wsdl_cache_ttl', 0);
+
 use \Taocomp\Sdicoop\Invoice;
 use \Taocomp\Sdicoop\Notification;
 
