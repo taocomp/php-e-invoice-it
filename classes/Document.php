b7e8d61e11437d5765724840e4412e119f4fa555
aded <aded@aded.it>
2018-12-11 06:15:03 +0100

    Add copyright

--- classes/Document.php
+++ classes/Document.php
@@ -1,4 +1,25 @@
-<?php namespace Taocomp\Sdicoop;
+<?php
+
+/**
+ * Copyright (C) 2018 Taocomp s.r.l.s. <https://taocomp.com>
+ *
+ * This file is part of php-sdicoop-invoice.
+ *
+ * php-sdicoop-invoice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * php-sdicoop-invoice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with php-sdicoop-invoice.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Taocomp\Sdicoop;
 
 class Document extends \SimpleXMLElement
 {
