c0f60b42c8dfb745131cfb64668608387d5d3b8b
aded <aded@aded.it>
2018-12-16 17:00:57 +0100

    Add example for sending invoices to SdI

---
+++ examples/send-invoice.php
@@ -0,0 +1,40 @@
+<?php
+
+use \Taocomp\EinvoiceIt\FatturaElettronica;
+use \Taocomp\Sdicoop\Client;
+
+try
+{
+    // Path to php-sdicoop-client
+    define('CLIENT_DIR', __DIR__ . '/../../php-sdicoop-client');
+    
+    require_once(__DIR__ . '/../autoload.php');
+    require_once(CLIENT_DIR . '/autoload.php');
+
+    // Create a new invoice
+    $invoice = new FatturaElettronica('FPR12');
+    $invoice->setValue('ProgressivoInvio', random_int(10000, 99999));
+    $invoice->setValues('IdTrasmittente', array(
+        'IdCodice' => '02313821007',
+        'IdPaese' => 'IT'
+    ));
+
+    // Setup client
+    Client::setPrivateKey(CLIENT_DIR . '/examples/client.key');
+    Client::setClientCert(CLIENT_DIR . '/examples/client.pem');
+    Client::setCaCert(CLIENT_DIR . '/examples/ca.pem');
+
+    $client = new Client(array(
+        'endpoint' => 'https://testservizi.fatturapa.it/ricevi_file',
+        'wsdl'     => CLIENT_DIR . '/wsdl/SdIRiceviFile_v1.0.wsdl'
+    ));
+
+    // Send invoice
+    $fileSdI = new \Taocomp\Sdicoop\FileSdIBase();
+    $fileSdI->load($invoice);
+    $response = new \Taocomp\Sdicoop\RispostaSdIRiceviFile($client->RiceviFile($fileSdI));    
+}
+catch (\Exception $e)
+{
+    echo $e->getMessage() . PHP_EOL;
+}
